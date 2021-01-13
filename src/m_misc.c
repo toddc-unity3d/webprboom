e99cbcdca7e1efb94102a9dd0c861cf507d71a0d
csmith <raz0red@gmail.com>
2021-01-13 12:44:09 -0800

    Modified default music volume

--- src/m_misc.c
+++ src/m_misc.c
@@ -296,7 +296,7 @@ default_t defaults[] =
    def_bool,ss_none}, // enables variable pitch in sound effects (from id's original code)
   {"samplerate",{&snd_samplerate},{22050},11025,48000, def_int,ss_none},
   {"sfx_volume",{&snd_SfxVolume},{8},0,15, def_int,ss_none},
-  {"music_volume",{&snd_MusicVolume},{8},0,15, def_int,ss_none},
+  {"music_volume",{&snd_MusicVolume},{15},0,15, def_int,ss_none},
   {"mus_pause_opt",{&mus_pause_opt},{2},0,2, // CPhipps - music pausing
    def_int, ss_none}, // 0 = kill music when paused, 1 = pause music, 2 = let music continue
   {"snd_channels",{&default_numChannels},{16},1,32, // ORIGINAL: 8
